34458d059e1c90986f14b3741aa813e79a2bb34c
Megan Yu <yu.megan@gmail.com>
2023-10-01 14:55:12 -0400

    firebase-config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 // import logo from './logo.svg';
 import './App.css';
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
 import Button from './components/Button';
 
 function App() {
@@ -15,21 +13,7 @@ function App() {
       <Button onClick = {handleClick}></Button>
     </div>
   );
+
 }
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAkQeokcnpcyhqzUX3_--AOeOCsaZ2fPe0",
-  authDomain: "jaba-fbac4.firebaseapp.com",
-  databaseURL: "https://jaba-fbac4-default-rtdb.firebaseio.com",
-  projectId: "jaba-fbac4",
-  storageBucket: "jaba-fbac4.appspot.com",
-  messagingSenderId: "155994837451",
-  appId: "1:155994837451:web:129ea10c78fc5c5867664e",
-  measurementId: "G-Q8RHXL45G8"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export default App;
